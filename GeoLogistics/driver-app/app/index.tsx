7001c75bce1eb606298eb1f4707766d88ccfc666
Pedro Wenner <pedro.alcantara@agendaassessoria.com.br>
2026-02-20 17:51:11 -0400

    app - 2

--- GeoLogistics/driver-app/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Redirect } from 'expo-router';
-
-export default function Index() {
-    // We will check authentication later. For now, redirect to login.
-    return <Redirect href="/(auth)/login" />;
-}
